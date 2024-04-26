b0b12872377ed1325b9b6ab7e310f0e6cbdb8fd9
lAirol <114059019+lAirol@users.noreply.github.com>
2024-04-26 06:49:30 +0300

    Подключение к firebase, создание окон логина/авторизации

---
+++ app/src/main/java/com/example/androidmessanger/RegisterActivity.java
@@ -0,0 +1,65 @@
+package com.example.androidmessanger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.androidmessanger.databinding.ActivityLoginBinding;
+import com.example.androidmessanger.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.singUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.emailEt.getText().toString().isEmpty() || binding.passwordEt.getText().toString().isEmpty()
+                        || binding.usernameEt.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                }else{
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.emailEt.getText().toString(), binding.passwordEt.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        HashMap<String, String> userInfo = new HashMap<>();
+                                        userInfo.put("email", binding.emailEt.getText().toString());
+                                        userInfo.put("username", binding.usernameEt.getText().toString());
+
+                                        FirebaseDatabase.getInstance().getReference().child("Users").child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                                                .setValue(userInfo);
+
+                                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                                    }
+                                }
+                            });
+
+                }
+            }
+        });
+    }
+}
