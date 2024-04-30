b5e0a1927f4a78fe65fcc16bbcec560db1a0b164
lAirol <114059019+lAirol@users.noreply.github.com>
2024-04-30 21:31:29 +0300

    Отображение пользователей онлайн

--- app/src/main/java/com/example/androidmessanger/bottomnav/new_chat/NewChatFragment.java
+++ app/src/main/java/com/example/androidmessanger/bottomnav/new_chat/NewChatFragment.java
@@ -4,13 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.androidmessanger.databinding.FragmentChatsBinding;
 import com.example.androidmessanger.databinding.FragmentNewChatBinding;
+import com.example.androidmessanger.users.User;
+import com.example.androidmessanger.users.UsersAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class NewChatFragment extends Fragment {
     private FragmentNewChatBinding binding;
@@ -19,6 +31,38 @@ public class NewChatFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentNewChatBinding.inflate(inflater,container,false);
+
+        loadUsers();
+
         return binding.getRoot();
+    }
+
+    private void loadUsers(){
+        ArrayList<User> users = new ArrayList<User>();
+        FirebaseDatabase.getInstance().getReference().child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot userSnapShot:snapshot.getChildren()){
+
+                    if(userSnapShot.getKey().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+                        continue;
+                    }
+
+                    String username =  userSnapShot.child("username").getValue().toString();
+                    String profileImage = userSnapShot.child("profileImage").getValue().toString();
+
+                    users.add(new User(username,profileImage));
+                }
+
+                binding.usersRv.setLayoutManager(new LinearLayoutManager(getContext()));
+                binding.usersRv.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
+                binding.usersRv.setAdapter(new UsersAdapter(users));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
