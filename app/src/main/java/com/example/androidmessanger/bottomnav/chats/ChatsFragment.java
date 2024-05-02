fdbd2c3a094b1450711f2ea8f6d5e6104d2e1489
lAirol <114059019+lAirol@users.noreply.github.com>
2024-05-02 03:23:39 +0300

    RecyclerView для всех

--- app/src/main/java/com/example/androidmessanger/bottomnav/chats/ChatsFragment.java
+++ app/src/main/java/com/example/androidmessanger/bottomnav/chats/ChatsFragment.java
@@ -8,8 +8,15 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.androidmessanger.chats.Chat;
+import com.example.androidmessanger.chats.ChatsAdapter;
 import com.example.androidmessanger.databinding.FragmentChatsBinding;
+
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.ArrayList;
 
 public class ChatsFragment extends Fragment {
     private FragmentChatsBinding binding;
@@ -18,6 +25,16 @@ public class ChatsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentChatsBinding.inflate(inflater,container,false);
+        loadChats();
         return binding.getRoot();
+    }
+
+    private void  loadChats(){
+        ArrayList<Chat> chats = new ArrayList<Chat>();
+        chats.add(new Chat("1233","tetqwe1","12312312313","12312312313"));
+        chats.add(new Chat("1232","tetqwe2","123123123132","123123123132"));
+        chats.add(new Chat("1231","tetqwe3","123123123133","123123123133"));
+        binding.chatsRv.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.chatsRv.setAdapter(new ChatsAdapter(chats));
     }
 }
